b3123a42f5faebe8f3eee8cbdc587c2e0461da1e
REBECCA L EVERETT <reverett2@wisc.edu>
2019-05-16 12:13:34 -0500

    switch to one file app

--- formapp/app.R
+++ formapp/app.R
@@ -1,3 +1,9 @@
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+  })
+}
+
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
@@ -6,3 +12,5 @@ ui <- fluidPage(
     mainPanel(plotOutput("distPlot"))
   )
 )
+
+shinyApp(ui = ui, server = server)
