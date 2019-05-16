cd686d701f52a70cde92154293dff85d94284f56
REBECCA L EVERETT <reverett2@wisc.edu>
2019-05-16 13:21:59 -0500

    setting up google sheet connection

--- formapp/app.R
+++ formapp/app.R
@@ -1,15 +1,36 @@
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
-  })
+library(shiny)
+library(googlesheets)
+
+#function signs a user into googlesheets
+getGoogleSheet <- function(){
+  gs_auth(new_user = TRUE)
+  theSheet <- gs_key('1nlOrDIa8rW1-VzMhk7DlRUKYXA3d-b-dDeYpiM0rVfU')
 }
 
+#will append a data tibble to a google sheet
+writeToSheet <- function(input){
+  
+}
+
+#server
+server <- function(input, output) {
+  
+}
+
+
+#ui
 ui <- fluidPage(
+#title
+  titlePanel("Data Entry Form for Data Sentencing Project"),
   sidebarLayout(
     sidebarPanel(
+      #inputs
       sliderInput("obs", "Number of observations:", min = 10, max = 500, value = 100)
     ),
-    mainPanel(plotOutput("distPlot"))
+    #main panel - will show record before sending to google docs sheet
+    mainPanel(
+      actionButton("connect","Connect to Google Sheet")
+      )
   )
 )
 
