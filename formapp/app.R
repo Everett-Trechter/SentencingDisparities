ae45e96e8e4249123ece8c0746c425d988d99072
Everett Trechter <everett.trechter@gmail.com>
2019-05-19 19:46:30 -0500

    starting things

--- formapp/app.R
+++ formapp/app.R
@@ -2,6 +2,9 @@ library(shiny)
 library(googlesheets)
 
 
+judges <- c("Flanagan, David T","Kawski, Clayton","Reynolds, Josann M","McNamara, Nichols J","Ehlke, Stephen E","Berz, Ellen K","Hanrahan, William","Hyland, John D","Hanson, Jason","Crawford, Susan M","O'Brien, Sarah B","Kaforsky, Jill J","Samuelson, Timothy C","Commissioner, First Appearance","Asmus, Brian")
+prosecutors <- c("Barnett, Paul L","McMiller, Tracy L ","Fallon, Thomas James JD ","Hilton, Stephanie R","Sattler, Rachel E","Larson, Cara J","Raymond, Andrea Beth ","Cogbill, Allison E","Liegel, Christopher Allen ","Quattromani, James Michael ","Wetjen, Chelsea E","Winter, Patrick","Hess, Daniel R ","Ozanne, Ismael R ","Brown, William Leroy ","Helmberger, Tim","Moeser, Matthew D ","Suleski, Justine L ","Jambois, Robert J ","Stephan, Corey","Miller, Jessica ","Kalser, Robert John Jr ","Hilton, Stephanie","Delain, Margaret ","Powell, Valerian ","Pierson, Bryce M ","Hanson, Erin","Hart, David A ","Khaleel, Awais ","Montano, Dallas B ","Keyes, Lexi","Rennicke, Rebekah M")
+
   options("googlesheets.webapp.client_id" = '4205699659-mjppbfcc54tpt44qgme6fsi9198dpmpg.apps.googleusercontent.com')
   options("googlesheets.webapp.client_secret" = 'bXyYWUThZN18tFWwSk0RYof4')
   options("googlesheets.webapp.redirect_uri" = "https://everettcarytrechter.shinyapps.io/formapp")
@@ -48,7 +51,12 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       #inputs
-     
+     textInput("caseno","Case Number"),
+     textInput("defendant","Defendant Name"),
+     radioButtons("Race","Race",c("AfAm","White")),
+     checkboxInput("hispanicName","Hispanic Seeming Name"),
+     selectInput("judge","Judge",judges),
+     selectInput("prosecutor","Prosecutor",prosecutors)
     ),
     #main panel - will show record before sending to google docs sheet
     mainPanel(
